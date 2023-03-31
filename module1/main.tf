0eed0595186875d2d1c91772f5750e95bb588696
Volodymyr Lapada <vlapada@>
2023-03-31 12:46:08 +0200

    updated repo

--- module1/main.tf
+++ module1/main.tf
@@ -3,9 +3,5 @@ resource "null_resource" "example" {}
 resource "null_resource" "example1" {}
 
 resource "null_resource" "example3" {}
-resource "null_resource" "example4" {}
-resource "null_resource" "example5" {}
-resource "null_resource" "example6" {}
-resource "null_resource" "example7" {}
-resource "null_resource" "example8" {}
+
 resource "null_resource" "example9" {}
