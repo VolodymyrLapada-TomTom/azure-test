fcff7f02255904a61841505e747f0fe11b794370
Volodymyr Lapada <vlapada@>
2023-03-31 16:03:26 +0200

    added new module

--- main/module2/main.tf
+++ main/module2/main.tf
@@ -4,8 +4,7 @@ resource "null_resource" "module2_example1" {}
 
 resource "null_resource" "module2_example2" {}
 
-resource "null_resource" "module2_example3" {}
-resource "null_resource" "module2_example4" {}
 
 resource "null_resource" "module2_example42" {}
+
 
